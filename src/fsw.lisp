3c05f72df406d2d351744a93874049c4075612c8
Walpurgisnatch <Kawemirs@gmail.com>
2022-10-03 10:46:53 +0700

    pwd ls mkdir

--- src/fsw.lisp
+++ src/fsw.lisp
@@ -1,7 +1,26 @@
 (in-package :cl-user)
 (defpackage fsw
-  (:use :cl))
+  (:use :cl
+   :fsw.utils)
+  (:export :pwd
+   :ls
+   :mkdir))
 
 (in-package :fsw)
 
 (cl-reexport:reexport-from :fsw.utils)
+
+(defun pwd ()
+  (uiop/os:getcwd))
+
+(defun ls (&optional directory)
+  (let ((dir (or directory ".")))
+    (when (wild-pathname-p dir)
+      (error "Wildcard not supported"))
+    (directory (directory-wildcard dir))))
+
+(defun mkdir (dir &optional parent)
+  (namestring (ensure-directories-exist
+               (if parent
+                   (merge-with-dir dir parent)
+                   (pathname-as-directory dir)))))
